a8cbbea0a2656558cd4f95e614492a6ee4acb2f6
Orlando D. A. R. Hill <orlandodarhill>
2008-11-16 01:32:34 +0100

    Improved build system syntax.

--- build/build.scm
+++ build/build.scm
@@ -28,60 +28,60 @@ mzscheme
 
 (define *name* "waxeye")
 (define *doc-book* "/usr/local/docbook")
-(define *gem-name* (string-append *name* "-" *version* ".gem"))
+(define *gem-name* (++ *name* "-" *version* ".gem"))
 
 
 (target clean (clean-book clean-gem clean-dist clean-unix)
-        (run ant "-f build/ant.xml clean-all")
-        (run rm "-rf tmp"))
+        (^ ant -f build/ant.xml clean-all)
+        (^ rm -rf tmp))
 
 
 (target book (book-html))
 
 
 (target book-html ()
-        (run asciidoc "-a toc -n -o docs/manual.html docs/book/book"))
+        (^ asciidoc -a toc -n -o docs/manual.html docs/book/book))
 
 
 (target book-pdf ()
-        (run mkdir "-p tmp/book")
-        (run asciidoc "-a toc -b docbook --doctype=book -o tmp/book/book.xml docs/book/book")
-        (run xsltproc "-o tmp/book/book.fo " *doc-book* "/fo/docbook.xsl tmp/book/book.xml")
-        (run fop "tmp/book/book.fo docs/manual.pdf"))
+        (^ mkdir -p tmp/book)
+        (^ asciidoc -a toc -b docbook --doctype=book -o tmp/book/book.xml docs/book/book)
+        ($ xsltproc '-o 'tmp/book/book.fo (++ *doc-book* "/fo/docbook.xsl") 'tmp/book/book.xml)
+        (^ fop tmp/book/book.fo docs/manual.pdf))
 
 
 (target clean-book ()
-        (run rm "-rf tmp/book")
-        (run rm "-f docs/manual.html docs/manual.pdf"))
+        (^ rm -rf tmp/book)
+        (^ rm -f docs/manual.html docs/manual.pdf))
 
 
 (target gem ()
-        (run mkdir "-p lib")
-        (run mkdir "-p tmp/gem/lib")
-        (run cp "src/ruby/waxeye.rb tmp/gem/lib/")
-        (run cp "docs/LICENSE tmp/gem/")
-        (run cp "README tmp/gem/")
-        (parameterize ((current-directory "tmp/gem"))
-                      (run gem "build ../../src/ruby/gem.gemspec"))
-        (run mv "tmp/gem/" *gem-name* " lib/"))
+        (^ mkdir -p lib)
+        (^ mkdir -p tmp/gem/lib)
+        (^ cp src/ruby/waxeye.rb tmp/gem/lib/)
+        (^ cp docs/LICENSE tmp/gem/)
+        (^ cp README tmp/gem/)
+        (cd tmp/gem
+            (^ gem build ../../src/ruby/gem.gemspec))
+        ($ mv (++ "tmp/gem/" *gem-name*) 'lib/))
 
 
 (target clean-gem ()
-        (run rm "-rf tmp/gem")
-        (run rm "-f lib/" *gem-name*))
+        (^ rm -rf tmp/gem)
+        ($ rm '-f (++ "lib/" *gem-name*)))
 
 
 (target dist (clean dist-src dist-unix))
 
 
 (define (cp-dist from)
-  (run cp "-r " from " dist/waxeye-" *version* "/"))
+  ($ cp '-r from (++ "dist/waxeye-" *version* "/")))
 
 
 (target dist-base (book gem)
 
- (run mkdir "-p dist/waxeye-" *version*)
- (run ant "-f build/ant.xml jar javadoc")
+ ($ mkdir '-p (++ "dist/waxeye-" *version*))
+ (^ ant -f build/ant.xml jar javadoc)
 
  (cp-dist "build")
  (cp-dist ".classpath")
@@ -93,31 +93,32 @@ mzscheme
  (cp-dist "src")
  (cp-dist "test")
 
- (run chmod "755 dist/waxeye-" *version* "/build/make")
- (run chmod "755 dist/waxeye-" *version* "/build/unix")
- (run chmod "755 dist/waxeye-" *version* "/build/waxeye"))
+ ($ chmod '755 (++ "dist/waxeye-" *version* "/build/make"))
+ ($ chmod '755 (++ "dist/waxeye-" *version* "/build/unix"))
+ ($ chmod '755 (++ "dist/waxeye-" *version* "/build/waxeye")))
 
 
 (target dist-src (dist-base)
-        (parameterize ((current-directory "dist"))
-                      (run zip "-r waxeye-" *version* "-src.zip waxeye-" *version*)
-                      (run tar "cjf waxeye-" *version* "-src.tar.bz2 waxeye-" *version*)))
+        (cd dist
+            ($ zip '-r (++ "waxeye-" *version* "-src.zip waxeye-" *version*))
+            ($ tar 'cjf (++ "waxeye-" *version* "-src.tar.bz2 waxeye-" *version*))))
 
 
 (target dist-unix (dist-base)
-        (parameterize ((current-directory (string-append "dist/waxeye-" *version*)))
-                      (system "./build/unix"))
-        (parameterize ((current-directory "dist"))
-                      (run tar "czf waxeye-" *version* "-unix.tar.gz waxeye-" *version*)
-                      (run tar "cjf waxeye-" *version* "-unix.tar.bz2 waxeye-" *version*)))
+        (cd$ (++ "dist/waxeye-" *version*)
+             (^ ./build/unix))
+        (cd dist
+            ($ tar 'czf (++ "waxeye-" *version* "-unix.tar.gz waxeye-" *version*))
+            ($ tar 'cjf (++ "waxeye-" *version* "-unix.tar.bz2 waxeye-" *version*))))
 
 
 (target clean-dist ()
-        (run rm "-rf dist"))
+        (^ rm -rf dist))
 
 
 (target clean-unix ()
-        (run rm "-rf bin lib"))
+        (^ rm -rf bin lib))
+
 
 (run-make)
 )
--- build/make.scm
+++ build/make.scm
@@ -24,16 +24,19 @@
 make
 mzscheme
 
-(require (lib "process.ss"))
-(provide target run run-make system)
+(require (only (lib "list.ss") foldr) (lib "process.ss"))
+(provide ^ $ ++ cd cd$ run-cmd run-make target)
 
 (define *target-table* (make-hash-table))
 (define *dep-table* (make-hash-table))
+
+(define ++ string-append)
 
 (define-syntax target
   (syntax-rules ()
     ((_ name (deps ...) code ...)
      (hash-table-put! *target-table* 'name (lambda () (for-each run-target '(deps ...)) code ...)))))
+
 
 (define (run-target t)
   (let ((t-code (hash-table-get *target-table* t #f)))
@@ -41,18 +44,57 @@ mzscheme
         (unless (hash-table-get *dep-table* t #f)
                 (hash-table-put! *dep-table* t #t)
                 (apply t-code ()))
-        (error 'make (string-append "target doesn't exist - " (symbol->string t))))))
+        (error 'make (++ "target doesn't exist - " (symbol->string t))))))
+
 
 (define (run-make)
   (for-each (lambda (a)
               (run-target (string->symbol a)))
             (vector->list (current-command-line-arguments))))
 
-(define-syntax run
+
+(define (run-cmd prog args)
+  (define (as-string s)
+    (cond
+     ((symbol? s) (symbol->string s))
+     ((char? s) (list->string (list s)))
+     ((number? s) (number->string s))
+     (else s)))
+  (let ((cmd (++ (as-string prog)
+                 (foldr (lambda (a b)
+                          (++ " " (as-string a) b))
+                        ""
+                        args))))
+    (display cmd)
+    (newline)
+    (system cmd)))
+
+
+(define-syntax $
   (syntax-rules ()
-    ((_ prog arg ...) (let ((cmd (string-append (symbol->string 'prog) " " arg ...)))
-                        (display cmd)
-                        (newline)
-                        (system cmd)))))
+    ((_ prog arg ...)
+     (run-cmd 'prog (list arg ...)))))
+
+
+(define-syntax ^
+  (syntax-rules ()
+    ((_ prog arg ...)
+     (run-cmd 'prog '(arg ...)))))
+
+
+(define-syntax cd$
+  (syntax-rules ()
+    ((_ dir code ...)
+     (parameterize ((current-directory (let ((d dir))
+                                         (if (symbol? d)
+                                             (symbol->string d)
+                                             d))))
+                   code ...))))
+
+
+(define-syntax cd
+  (syntax-rules ()
+    ((_ dir code ...)
+     (cd$ 'dir code ...))))
 
 )
