33e2d0dba2c68d0c6e215c66985c010069f97316
Orlando D. A. R. Hill <orlandodarhill>
2008-11-18 20:58:19 +0100

    Wrote arrow printing for python ASTs.

--- src/python/waxeye.py
+++ src/python/waxeye.py
@@ -12,16 +12,23 @@ class AST:
             sys.stdout.write('    ')
         if indent[0] > 0:
             sys.stdout.write('->  ')
-        print self.type
+        print ast.type
         indent[0] += 1
+        for a in ast.children:
+            if isinstance(a, AST):
+                self.display_iter(a, indent)
+            else:
+                for i in range(0, indent[0] - 1):
+                    sys.stdout.write('    ')
+                if indent[0] > 0:
+                    sys.stdout.write('|   ')
+                print a
+        indent[0] -= 1
 
     def display(self):
-        self.display_iter(self, [3])
+        self.display_iter(self, [0])
 
-
-
-
-
-a = AST("Hello Mama!", ['a', 'b', 'c'], [0, 2])
+c = AST("num", ['1', '2'], [0, 2])
+a = AST("Hello Mama!", ['a', c, 'c'], [0, 4])
 
 a.display()
