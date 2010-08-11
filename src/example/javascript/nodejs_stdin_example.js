fec86ba50f820e75d79549ce26391469590aad5d
Orlando Hill <spam@odarh.com>
2010-08-11 15:04:39 +0200

    Add STDIN example for Node.js

---
+++ src/example/javascript/nodejs_stdin_example.js
@@ -0,0 +1,46 @@
+/**
+* Waxeye Parser Generator
+* www.waxeye.org
+* Copyright (C) 2008 Orlando D. A. R. Hill
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy of
+* this software and associated documentation files (the "Software"), to deal in
+* the Software without restriction, including without limitation the rights to
+* use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+* of the Software, and to permit persons to whom the Software is furnished to do
+* so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+**/
+var sys = require('sys');
+var parser = require('./parser');
+
+var input = "";
+var stdin = process.openStdin();
+
+stdin.setEncoding('utf8');
+
+// Read our input
+stdin.on('data', function (chunk) {
+    input += chunk;
+});
+
+stdin.on('end', function () {
+    // Create our parser
+    var p = new parser.Parser();
+
+    // Parse our input
+    var ast = p.parse(input);
+
+    // Print our AST
+    sys.puts(ast);
+});
