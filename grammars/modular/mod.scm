e1b3692326d527acc9cd9c00e2979b9c1a775678
Orlando Hill <spam@odarh.com>
2010-08-29 19:36:50 +0200

    remove headers from example grammars

--- grammars/modular/mod.scm
+++ grammars/modular/mod.scm
@@ -1,9 +1,3 @@
-;; Waxeye Parser Generator
-;; www.waxeye.org
-;; Copyright (C) 2008-2010 Orlando Hill
-;; Licensed under the MIT license. See 'LICENSE' for details.
-
-
 ;; A contrived example where we replace the definition of Number in Json with a
 ;; much simpler one that only supports integers.
 
