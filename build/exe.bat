38f5245786c016617adef2aa36810313474184c1
Orlando Hill <spam@odarh.com>
2012-03-15 21:13:07 +0100

    refer to Racket instead of PLT-Scheme

--- build/exe.bat
+++ build/exe.bat
@@ -1,6 +1,6 @@
 
 MKDIR tmp
-C:\"Program Files\PLT\mzc.exe" --exe waxeye src\waxeye\waxeye.scm
+C:\"Program Files\Racket\mzc.exe" --exe waxeye src\waxeye\waxeye.scm
 MOVE waxeye.exe tmp\waxeye.exe
-C:\"Program Files\PLT\mzc.exe" --exe-dir . tmp\waxeye.exe
+C:\"Program Files\Racket\mzc.exe" --exe-dir . tmp\waxeye.exe
 RMDIR /S tmp
