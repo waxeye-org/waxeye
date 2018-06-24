5bb432342b581917a03db1921dac70ec3c39ddf7
Gleb Mazovetskiy <glex.spb@gmail.com>
2018-06-24 18:34:46 +0100

    Update test-javascript for ts-node v7 compat

--- test/bin/lib-javascript.sh
+++ test/bin/lib-javascript.sh
@@ -4,7 +4,7 @@ source "${BASH_SOURCE%/*}/lib-waxeye.sh"
 : ${NODE_BIN:="$(which node || which nodejs || log_error Please install nodejs && false)"}
 : ${NODE_CMD:="env NODE_PATH=src/javascript/" "$NODE_BIN"}
 : ${TS_NODE_BIN:="src/javascript/node_modules/.bin/ts-node"}
-: ${TS_NODE_CMD:="env NODE_PATH=src/javascript/" "$TS_NODE_BIN" "--project test/javascript"}
+: ${TS_NODE_CMD:="env NODE_PATH=src/javascript/" "$TS_NODE_BIN" "--project test/javascript/tsconfig.json"}
 
 gen_javascript_parser() {
   rm -rf tmp/js/test_parser.js
