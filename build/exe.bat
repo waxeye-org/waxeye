daa6a43027b755244fabac25ff29cdab03cf5d2a
Gleb Mazovetskiy <glex.spb@gmail.com>
2018-06-26 13:12:01 +0100

    Fix invalid path in build/exe.bat

--- build/exe.bat
+++ build/exe.bat
@@ -1,4 +1,4 @@
-SET PLTCOLLECTS=";%~dp0..\src\scheme\"
+SET PLTCOLLECTS=";%~dp0..\src\racket\"
 "C:\Program Files\Racket\raco.exe" exe "%~dp0..\src\waxeye\waxeye.rkt"
 "C:\Program Files\Racket\raco.exe" distribute . "%~dp0..\src\waxeye\waxeye.exe"
 DEL "%~dp0..\src\waxeye\waxeye.exe"
