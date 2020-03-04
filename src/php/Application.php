22cb12b3e018280894468979d4ecee080647e68c
Michael Becker <michael.becker@caterdev.de>
2020-03-04 18:41:47 +0100

    php generation

--- src/php/Application.php
+++ src/php/Application.php
@@ -1,122 +1,12 @@
 <?php
 
-use parser\AutomatonTransition;
-use parser\CharTransition;
-use parser\Edge;
-use parser\Edges;
-use parser\FA;
-use parser\FAs;
-use parser\Parser;
-use parser\State;
-use parser\States;
-use parser\WildcardTransition;
-use util\CharArray;
 
 spl_autoload_register(function ($class_name) {
     include $class_name . ".php";
 });
 
 
-class TestParser extends Parser
-{
-    public function __construct()
-    {
-        $fas = new FAs();
-
-        // CWR File
-        $states = new States();
-        $edges = new Edges();
-        $edges->append(new Edge(new AutomatonTransition(1), 1, false));
-        $states->append(new State($edges, false));
-
-        $edges = new Edges();
-        $edges->append(new Edge(new AutomatonTransition(2), 2, false));
-        $states->append(new State($edges, false));
-
-        $edges = new Edges();
-        $edges->append(new Edge(new AutomatonTransition(3), 3, false));
-        $states->append(new State($edges, false));
-
-        $edges = new Edges();
-        $states->append(new State($edges, true));
-        $fas[] = new FA("CWRFile", $states);
-
-
-        // TransmissionHeader
-        $states = new States();
-        $edges = new Edges();
-        $edges->append(new Edge(new CharTransition(new CharArray('X'), new CharArray(), new CharArray()), 1, false));
-        $edges->append(new Edge(new CharTransition(new CharArray('H'), new CharArray(), new CharArray()), 1, false));
-        $states->append(new State($edges, false));
-
-        $edges = new Edges();
-        $edges->append(new Edge(new CharTransition(new CharArray('D'), new CharArray(), new CharArray()), 2, false));
-        $states->append(new State($edges, false));
-
-        $edges = new Edges();
-        $edges->append(new Edge(new CharTransition(new CharArray('R'), new CharArray(), new CharArray()), 3, false));
-        $states->append(new State($edges, false));
-
-        $edges = new Edges();
-        $states->append(new State($edges, true));
-        $fas[] = new FA("TransmissionHeader", $states);
-
-
-        // SenderType
-        $states = new States();
-        $edges = new Edges();
-        $edges->append(new Edge(new CharTransition(new CharArray("P"), new CharArray(), new CharArray()), 1, false));
-        $states->append(new State($edges, false));
-
-        $edges = new Edges();
-        $edges->append(new Edge(new CharTransition(new CharArray("B"), new CharArray(), new CharArray()), 2, false));
-        $states->append(new State($edges, false));
-
-        $edges = new Edges();
-        $states->append(new State($edges, true));
-        $fas[] = new FA("SenderType", $states);
-
-        $states = new States();
-        $edges = new Edges();
-        $edges->append(new Edge(new WildcardTransition(), 1, false));
-        $states->append(new State($edges, false));
-        $edges = new Edges();
-        $edges->append(new Edge(new WildcardTransition(), 2, false));
-        $states->append(new State($edges, false));
-        $edges = new Edges();
-        $edges->append(new Edge(new WildcardTransition(), 3, false));
-        $states->append(new State($edges, false));
-        $edges = new Edges();
-        $edges->append(new Edge(new WildcardTransition(), 4, false));
-        $states->append(new State($edges, false));
-        $edges = new Edges();
-        $edges->append(new Edge(new WildcardTransition(), 5, false));
-        $states->append(new State($edges, false));
-        $edges = new Edges();
-        $edges->append(new Edge(new WildcardTransition(), 6, false));
-        $states->append(new State($edges, false));
-        $edges = new Edges();
-        $edges->append(new Edge(new WildcardTransition(), 7, false));
-        $states->append(new State($edges, false));
-        $edges = new Edges();
-        $edges->append(new Edge(new WildcardTransition(), 8, false));
-        $states->append(new State($edges, false));
-        $edges = new Edges();
-        $edges->append(new Edge(new WildcardTransition(), 9, false));
-        $states->append(new State($edges, false));
-        $edges = new Edges();
-        $states->append(new State($edges, true));
-        $fas[] = new FA("SenderId", $states);
-
-
-        printf("%s\n", $fas);
-
-        parent::__construct($fas);
-    }
-}
-
-
-$parser = new TestParser();
+$parser = new WaxeyeParser();
 $result = $parser->parse("HDRPB512590964");//KICK  THE FLAME MUSIKVERLAG, RAJK BARTHEL    01.102019112512513620191125               ");
 printf("%s\n", $result);
 
